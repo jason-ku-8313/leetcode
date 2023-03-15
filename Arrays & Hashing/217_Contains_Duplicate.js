326b98de2496f24ed2911501d50e75ed5d6d39e6
Jason Ku <cuhaoyan@gmail.com>
2023-03-15 13:33:29 +0800

    leetcode 242: Valid Anagram

--- Arrays & Hashing/217_Contains_Duplicate.js
+++ Arrays & Hashing/217_Contains_Duplicate.js
@@ -4,13 +4,17 @@
  * Given an integer array nums, return true if any value appears at least twice in the array,
  * and return false if every element is distinct.
  *
+ * Constraints:
+ * 1. 1 <= nums.length <= 105
+ * 2. -109 <= nums[i] <= 109
+ *
  * @param {number[]} nums
  * @return {boolean}
  */
 
-// Time: O(nlogn), Space: O(1)
+// Time: O(n^2), Space: O(1)
 var containsDuplicate_bruteForce = function (nums) {
-  // Nested for-loop: T=O(nlogn)
+  // Nested for-loop: T=O(n^2)
   for (let i = 0; i < nums.length; i++) {
     for (let y = 1; y < nums.length; y++) {
       if (nums[i] === nums[y]) {
@@ -21,9 +25,9 @@ var containsDuplicate_bruteForce = function (nums) {
   return false;
 };
 
-// Time: O(logn), Space: O(1)
+// Time: O(nlogn), Space: O(1)
 var containsDuplicate_quicksort = function (nums) {
-  nums.sort(); // Quick Sort: T=O(logn)
+  nums.sort(); // Quick Sort: T=O(nlogn)
   for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] === nums[i + 1]) {
       return true;
---
+++ Arrays & Hashing/242_Valid_Anagram.js
@@ -0,0 +1,37 @@
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Constraints:
+ * 1. 1 <= s.length, t.length <= 5 * 104
+ * 2. s and t consist of lowercase English letters.
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// Time: O(nlogn), Space: O(1)
+var isAnagram_quicksort = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const sortedS = [...s].sort().join(""); // T=O(nlogn)
+  const sortedT = [...t].sort().join(""); // T=O(nlogn)
+  return sortedS === sortedT;
+};
+
+// Time: O(n), Space: O(26)
+var isAnagram_ASCII_code = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const arr = new Array(26).fill(0);
+  const a = "a".charCodeAt(); // 97
+  // T=O(n)
+  for (let i = 0; i < s.length; i++) {
+    arr[s[i].charCodeAt() - a]++;
+    arr[t[i].charCodeAt() - a]--;
+  }
+
+  return arr.every((num) => num === 0); // T=O(n)
+};
