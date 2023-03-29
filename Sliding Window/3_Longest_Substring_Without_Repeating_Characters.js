cd0a3ea694c12d33946a2f0c40d6282a268c13e6
Jason Ku <cuhaoyan@gmail.com>
2023-03-29 22:00:30 +0800

    leetcode 3: Longest_Substring_Without_Repeating_Characters.js

--- Sliding Window/3_Longest_Substring_Without_Repeating_Characters.js
+++ Sliding Window/3_Longest_Substring_Without_Repeating_Characters.js
@@ -12,37 +12,15 @@
 var lengthOfLongestSubstring_map_slidingWindow = function (s) {
   let maxLen = 0;
   let left = 0;
-  let right = 0;
-  let map = new Map(); // S=O(n)
-
-  // T=O(n)
-  while (right < s.length) {
-    if (map.has(s[right])) {
-      maxLen = Math.max(map.size, maxLen);
-      left = map.get(s[right]) + 1;
-      right = left;
-      map.clear();
-    }
-    map.set(s[right], right);
-    right++;
-  }
-  return Math.max(map.size, maxLen);
-};
-
-// Time: O(n), Space: O(n)
-var lengthOfLongestSubstring_fasterSolution = function (s) {
-  let maxLen = 0;
-  let left = 0;
   let map = new Map(); // S=O(n)
 
   // T=O(n)
   for (let i = 0; i < s.length; i++) {
-    while (map.has(s[i])) {
-      map.delete(s[left]);
-      left++;
+    if (map.has(s[i])) {
+      left = Math.max(map.get(s[i]) + 1, left);
     }
-    map.set(s[i]);
     maxLen = Math.max(i - left + 1, maxLen);
+    map.set(s[i], i);
   }
   return maxLen;
 };
