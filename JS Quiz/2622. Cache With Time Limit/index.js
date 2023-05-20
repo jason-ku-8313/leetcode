9ccec8f11c158ddb5475943abe67d15003a42269
Jason Ku <cuhaoyan@gmail.com>
2023-05-20 21:19:25 +0800

    leetcode problems 2622: Cache With Time Limit

---
+++ JS Quiz/2622. Cache With Time Limit/index.js
@@ -0,0 +1,49 @@
+var TimeLimitedCache = function () {
+  this.cache = new Map();
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  // Remove previous timer to o prevent new value be removed
+  if (this.cache.has(key)) {
+    const { timer } = this.cache.get(key);
+    clearTimeout(timer);
+  }
+
+  const timer = setTimeout(() => {
+    this.cache.delete(key);
+  }, duration);
+  this.cache.set(key, { timer, value });
+  return hasKey;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) {
+    return -1;
+  }
+  return this.cache.get(key).value;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
