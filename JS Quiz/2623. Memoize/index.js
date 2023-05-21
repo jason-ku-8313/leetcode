50fcd506fd501414315998bcff435f60ee07310a
Jason Ku <cuhaoyan@gmail.com>
2023-05-21 15:30:12 +0800

    leetcode problems 2623: Memoize

---
+++ JS Quiz/2623. Memoize/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+  const cache = new Map();
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
